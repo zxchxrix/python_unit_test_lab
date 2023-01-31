371e2f82c927fde9100997ea017f1c454273de62
clara <10088152+claraj@users.noreply.github.com>
2023-01-31 09:35:53 -0600

    update magic 8 ball URL

--- magic_8_ball/functions_magic.py
+++ magic_8_ball/functions_magic.py
@@ -10,7 +10,7 @@ def main():
 
 
 def generate_url_for_question(question):
-    url = f'https://8ball.delegator.com/magic/JSON/{question}'
+    url = f'https://magic-eight-ball.azurewebsites.net/magic/JSON/{question}'
     return url
 
 
@@ -27,11 +27,9 @@ def extract_answer_from_response(response):
     The response is a dictionary in the form
 
     {
-        'magic': {
-            'question': 'Will it be sunny tomorrow?', 
-            'answer': 'Yes', 
-            'type': 'Affirmative'
-        }
+        'question': 'Will it be sunny tomorrow?', 
+        'answer': 'Yes', 
+        'type': 'Affirmative'
     }
 
     This function returns the answer value from the nested dictionary.
@@ -40,7 +38,7 @@ def extract_answer_from_response(response):
     # TODO what would happen if the response dictionary was not in the expected form?
     # TODO can you modify this function to print an error message, and return None
     #   if the response dictionary is not in this structure? 
-    answer = response['magic']['answer']
+    answer = response['answer']
     return answer
 
 
--- magic_8_ball/original_magic.py
+++ magic_8_ball/original_magic.py
@@ -2,10 +2,10 @@ import requests
 
 question = input('Enter your question for the magic 8 ball: ')
 
-magic_8_ball_url = f'https://8ball.delegator.com/magic/JSON/{question}'
+magic_8_ball_url = f'https://magic-eight-ball.azurewebsites.net/magic/JSON/{question}'
 
 response = requests.get(magic_8_ball_url).json()
 
-answer = response['magic']['answer']
+answer = response['answer']
 
 print(f'The magic 8 ball says....  {answer}')
